033a10de1d4fe91ee0edd068b8f7f67eecaba5e4
rgaw-pon <38362537+rgaw-pon@users.noreply.github.com>
2020-10-31 22:47:07 +0100

    various updates

---
+++ Game/superGiera/Assets/Scripts/GlobalControl.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class GlobalControl : MonoBehaviour
+{
+    public static GlobalControl Instance;
+    public int currentRow;
+    public List<string> visitedNodes;
+    public ArrayList allNodes = new ArrayList();
+
+    void Awake()
+    {
+        allNodes.AddRange(new[] { "Start" });
+        allNodes.AddRange(new[] { "1A", "1B", "1C" });
+        allNodes.AddRange(new[] { "2A", "2B" });
+        allNodes.AddRange(new[] { "3" });
+        allNodes.AddRange(new[] { "4A", "4B" });
+        allNodes.AddRange(new[] { "5A", "5B", "5C" });
+        allNodes.AddRange(new[] { "Boss"});
+
+        if (Instance == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Game/superGiera/Assets/Scripts/MapScript.cs
+++ Game/superGiera/Assets/Scripts/MapScript.cs
@@ -4,56 +4,95 @@ using UnityEngine;
 
 public class MapScript : MonoBehaviour
 {
-public void startClick()
+    public void startClick()
     {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(2);
+        GlobalControl.Instance.visitedNodes.Add("Start");
+        GlobalControl.Instance.currentRow = 1;
 
     }
     public void OneAClick()
     {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(2);
+        GlobalControl.Instance.visitedNodes.Add("1A");
+        GlobalControl.Instance.currentRow++;
 
     }
     public void OneBClick()
     {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(2);
+        GlobalControl.Instance.visitedNodes.Add("1B");
+        GlobalControl.Instance.currentRow++;
 
     }
     public void OneCClick()
     {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(2);
+        GlobalControl.Instance.visitedNodes.Add("1C");
+        GlobalControl.Instance.currentRow++;
 
     }
     public void TwoAClick()
     {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(2);
+        GlobalControl.Instance.visitedNodes.Add("2A");
+        GlobalControl.Instance.currentRow++;
 
     }
     public void TwoBClick()
     {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(2);
+        GlobalControl.Instance.visitedNodes.Add("2B");
+        GlobalControl.Instance.currentRow++;
 
     }
     public void ThreeClick()
     {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(2);
+        GlobalControl.Instance.visitedNodes.Add("3");
+        GlobalControl.Instance.currentRow++;
 
     }
     public void FourAClick()
     {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(2);
+        GlobalControl.Instance.visitedNodes.Add("4A");
+        GlobalControl.Instance.currentRow++;
 
     }
     public void FourBClick()
     {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(2);
+        GlobalControl.Instance.visitedNodes.Add("4B");
+        GlobalControl.Instance.currentRow++;
 
     }
     public void FiveAClick()
     {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(2);
+        GlobalControl.Instance.visitedNodes.Add("5A");
+        GlobalControl.Instance.currentRow++;
 
     }
     public void FiveBClick()
     {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(2);
+        GlobalControl.Instance.visitedNodes.Add("5B");
+        GlobalControl.Instance.currentRow++;
 
     }
     public void FiveCClick()
     {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(2);
+        GlobalControl.Instance.visitedNodes.Add("5C");
+        GlobalControl.Instance.currentRow++;
 
     }
     public void BossClick()
     {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(2);
+        GlobalControl.Instance.visitedNodes.Add("Boss");
+        GlobalControl.Instance.currentRow=0;
 
     }
 }
