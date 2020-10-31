fb9342ba54926e9d9027f7f1bf7e16638e9392ad
Stokon <47858617+Stokon@users.noreply.github.com>
2020-11-01 00:35:23 +0100

    commitment

---
+++ Game/superGiera/Assets/Fight.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum State {START, PTURN,ETURN,WIN,LOSS }
+public class Fight : MonoBehaviour
+{
+    public GameObject playerPrefab;
+    public GameObject enemyPrefab;
+
+    public Transform playerBS;
+    public Transform enemyBS;
+
+    public State fightState;
+    void Start()
+    {
+        fightState = State.START;
+        SetupFight();
+    }
+    void SetupFight()
+    {
+        Instantiate(playerPrefab, playerBS);
+        Instantiate(enemyPrefab, enemyBS);
+    }
+}
---
+++ Game/superGiera/Assets/Unit.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    public string name;
+    public int level;
+    public int dmg;
+    public int hpMax;
+    public int hpCurr;
+
+}
