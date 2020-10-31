221207d07743847c67b61a49ea35a09b526ec5f2
rgaw-pon <38362537+rgaw-pon@users.noreply.github.com>
2020-10-31 20:43:11 +0100

    method calls for buttons

---
+++ Game/superGiera/Assets/Scripts/MapScript.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapScript : MonoBehaviour
+{
+public void startClick()
+    {
+
+    }
+    public void OneAClick()
+    {
+
+    }
+    public void OneBClick()
+    {
+
+    }
+    public void OneCClick()
+    {
+
+    }
+    public void TwoAClick()
+    {
+
+    }
+    public void TwoBClick()
+    {
+
+    }
+    public void ThreeClick()
+    {
+
+    }
+    public void FourAClick()
+    {
+
+    }
+    public void FourBClick()
+    {
+
+    }
+    public void FiveAClick()
+    {
+
+    }
+    public void FiveBClick()
+    {
+
+    }
+    public void FiveCClick()
+    {
+
+    }
+    public void BossClick()
+    {
+
+    }
+}
